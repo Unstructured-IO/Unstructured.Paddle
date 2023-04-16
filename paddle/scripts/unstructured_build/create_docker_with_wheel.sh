94ce21792c37737549d97f5414448230bd8d92fc
Ryan Nikolaidis <1208590+ryannikolaidis@users.noreply.github.com>
2023-04-16 12:28:47 -0700

    amd build

--- paddle/scripts/unstructured_build/create_docker_with_wheel.sh
+++ paddle/scripts/unstructured_build/create_docker_with_wheel.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 DOCKER_REPOSITORY="${DOCKER_REPOSITORY:-quay.io/unstructured-io/paddle}"
 PIP_VERSION="${PIP_VERSION:-22.2.1}"
 DOCKER_IMAGE="${DOCKER_IMAGE:-paddle}"
-DOCKER_FILE="paddle/scripts/unstructured_build/manylinux_arm.dockerfile"
+DOCKER_FILE="paddle/scripts/unstructured_build/manylinux_amd.dockerfile"
 
 DOCKER_BUILD_CMD=(docker buildx build --load \
   --build-arg PIP_VERSION="$PIP_VERSION" \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
-  --platform linux/arm64 \
+  --platform linux/amd64 \
   --progress plain \
   --cpuset-cpus=2 --memory=4g --memory-swap=4g \
   --ulimit nofile=8192 --ulimit fsize=10000000 \
-  --cache-from "$DOCKER_REPOSITORY":arm \
+  --cache-from "$DOCKER_REPOSITORY":amd \
   -t "$DOCKER_IMAGE" -f "./$DOCKER_FILE" .)
 
 # only build for specific platform if DOCKER_BUILD_PLATFORM is set
